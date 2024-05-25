761c1212df24efb4056c7dc5be1e502f5ce24606
Preetham Pemmasani <75422607+ppmpreetham@users.noreply.github.com>
2024-05-25 21:32:24 +0530

    Update yProblem_65.py

--- src/yProblem_65.py
+++ src/yProblem_65.py
@@ -1,4 +1,6 @@
 def recursive_fraction(n):
     flag=0
     flag+=1
-    return 1/(1+recursive_fraction(n)) if flag!=n else 
+    return 1/(1+recursive_fraction(n)) if flag!=n else n
+
+recursive_fraction(12)
